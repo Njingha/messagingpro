8cf15c845743b943109db875aaabe1669106fdbc
Kenny <kenny.wayne972@gmail.com>
2023-11-16 23:15:37 +0100

    configuration initiale de la base de l'app

---
+++ src/main/java/com/example/messagingpro/config/ApplicationConfig.java
@@ -0,0 +1,18 @@
+package com.example.messagingpro.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.messagingpro")
+public class ApplicationConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        super.addArgumentResolvers(argumentResolvers);
+    }
+}
